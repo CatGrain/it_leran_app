df7a1efc086582d6dad06782a55dfe61e325e941
CatGrain <catgrainstudio@gmail.com>
2023-11-07 21:39:00 +0100

    update

--- app/src/main/java/com/example/it_learn_app/MainActivity.kt
+++ app/src/main/java/com/example/it_learn_app/MainActivity.kt
@@ -2,16 +2,15 @@ package com.example.it_learn_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.it_learn_app.model.StateManager.ViewState
+import android.provider.SyncStateContract.Helpers.update
+import androidx.constraintlayout.motion.utils.ViewState
 import com.example.it_learn_app.databinding.ActivityMainBinding
 import com.example.it_learn_app.model.QuestionManager
-import com.example.it_learn_app.model.StateManager
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
-    private val stateManager = StateManager()
+    private lateinit var binding: ActivityMainBinding
     private val questionManager: QuestionManager = QuestionManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,33 +18,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        update()
 
         binding.enterButton.setOnClickListener {
-            update()
-        }
-    }
 
-    private fun update(){
-        stateManager.updateState()
 
-        when (stateManager.getState()) {
-
-            ViewState.ANSWERQUESTION -> {
-                questionManager.nextQuestion()
-                binding.questionDescription.text = questionManager.curentQuestion.question;
-                binding.enterButton.text = "weiter"
-                binding.questionAnswerResult.text = ""
-            }
-
-            ViewState.QUESTIONRESULT -> {
-                if (isAnswerCorect()){
-                    binding.questionAnswerResult.text = "Richtig"
-                } else {
-                    binding.questionAnswerResult.text = questionManager.curentQuestion.answer;
-                }
-            }
-
+            
         }
     }
 
